4ecb3d5cf0c8fbe31edc17c85a854baa5b976134
fiona-grimm <Fiona.grimm@health.org.uk>
2020-05-19 10:52:02 +0100

    add graph showing % from expected

--- src/sprint2/04_combined_vis.R
+++ src/sprint2/04_combined_vis.R
@@ -138,9 +138,14 @@ region_order_COVID <- order_table_COVID$region[order(order_table_COVID$value)]
 
 # maps showing deaths/bed
 
+region_combined_wide <- region_combined %>% 
+  pivot_wider(names_from = "var", values_from = "value") %>% 
+  mutate(deaths_cum_covid_expected = sum(deaths_cum_covid) * (percent_beds/100),
+         deaths_cum_covid_excess_pct = round(100*((deaths_cum_covid/deaths_cum_covid_expected)-1),1))
+
 region_shape <- regions_json_df %>% 
-  left_join(region_combined %>% 
-              pivot_wider(names_from = "var", values_from = "value"), by = c("id" = "region"))
+  left_join(region_combined_wide, 
+            by = c("id" = "region"))
 
 # Plot number of deaths and deaths per bed per region
 (region_shape %>% 
@@ -192,3 +197,30 @@ region_shape <- regions_json_df %>%
          subtitle = "Beds: April 2020, Deaths: 28 Dec, 2019 - May 1, 2020", 
          caption = "Source: ONS")) %>% 
   ggsave("graphs/sprint_2/Care_home_beds_COVID_deaths_map.png", ., width = 5, height = 5)
+
+# percent above/below expected number of deaths, based on number of beds
+(region_shape %>% 
+    ggplot(aes(x = long, y = lat, group = group, fill = deaths_cum_covid_excess_pct)) +
+    geom_polygon(colour = "grey80") +
+    theme_void() +
+    coord_map() +
+    scale_fill_gradient2(low = THF_50pct_light_blue, mid = "white", high = THF_red) +
+    labs(title = "COVID-19 deaths in care home residents\n% excess over expected", 
+         subtitle = "Beds: April 2020, Deaths: 28 Dec, 2019 - May 1, 2020", 
+         caption = "Source: ONS")) %>% 
+  ggsave("graphs/sprint_2/Care_home_beds_COVID_deaths_excess_map.png", ., width = 5, height = 5)
+
+(region_combined_wide %>% 
+    ggplot(aes(x = fct_reorder(region, deaths_cum_covid_excess_pct), y = deaths_cum_covid_excess_pct)) +
+    geom_bar(stat = "identity", position = position_dodge(), width = 0.8, fill = THF_red) +
+    theme_bw() +
+    ylab("% above/below expected COVID deaths based on the number of care home beds") +
+    coord_flip()+
+    theme(axis.title.y = element_blank(),
+          legend.position = "none",
+          panel.grid.major.x = element_blank()) +
+    geom_hline(yintercept = 0) +
+    labs(title = "COVID-19 deaths in care home residents\n% excess over expected", 
+         subtitle = "Beds: April 2020, Deaths: 28 Dec, 2019 - May 1, 2020", 
+         caption = "Source: ONS")) %>% 
+  ggsave("graphs/sprint_2/Care_home_beds_COVID_deaths_excess_bar.png", ., width = 7, height = 5)
