b6fb65a2233eddb99a2cf6217637d3b67eea21e5
fiona-grimm <Fiona.grimm@health.org.uk>
2020-07-01 10:15:45 +0000

    Add graphs for absolute values and comparison to 2019 only

--- src/sprint_3/01_admissions_discharges.R
+++ src/sprint_3/01_admissions_discharges.R
@@ -336,7 +336,9 @@ discharges_april_excldeaths <- discharges_april_excldeaths %>%
 discharges_april_excldeaths <- discharges_april_excldeaths %>%
   group_by(type, sourcedest, day_dummy) %>%
   mutate(ref_2015_to_2019 = mean(sevendayavg[year %in% c(2015:2019)]),
-         percent_2015_to_2019 = round(100*sevendayavg / ref_2015_to_2019, 1)) 
+         percent_2015_to_2019 = round(100*sevendayavg / ref_2015_to_2019, 1),
+         ref_2019 = mean(sevendayavg[year == 2019]),
+         percent_2019 = round(100*sevendayavg / ref_2019, 1)) 
 
 
 ## Admissions
@@ -368,23 +370,103 @@ admissions_april_excldeaths <- admissions_april_excldeaths %>%
 admissions_april_excldeaths <- admissions_april_excldeaths %>%
   group_by(type, sourcedest, day_dummy) %>%
   mutate(ref_2015_to_2019 = mean(sevendayavg[year %in% c(2015:2019)]),
-         percent_2015_to_2019 = round(100*sevendayavg / ref_2015_to_2019, 1)) 
+         percent_2015_to_2019 = round(100*sevendayavg / ref_2015_to_2019, 1),
+         ref_2019 = mean(sevendayavg[year == 2019]),
+         percent_2019 = round(100*sevendayavg / ref_2019, 1)) 
 
 # Admissions and discharges in a combined graph ----
 
 combined_april_excldeaths <- admissions_april_excldeaths %>% 
-  bind_rows(discharges_april_excldeaths) %>%
-  filter(day_dummy <= ymd("0004-04-30") & day_dummy >= ymd("0004-02-01") & sourcedest != "all" & year == "2020")
+  bind_rows(discharges_april_excldeaths) 
 
 combined_april_excldeaths %>% 
+  filter(day_dummy <= ymd("0004-04-30") & day_dummy >= ymd("0004-02-01") & sourcedest != "all" & year == "2020")
   ungroup() %>% 
   select(-ref_2015_to_2019, -value, -sevendayavg, -year, -day_dummy) %>% 
   unite("combined_col", type, sourcedest) %>% 
   pivot_wider(names_from = "combined_col", values_from = "percent_2015_to_2019") %>% 
+  rename(pct_admissions_from_carehomes = "adm_ch", pct_admissions_from_other_sources = "adm_other",
+         pct_discharges_to_carehomes = "disch_ch", pct_discharges_to_other_destinations = "disch_other") %>% 
   write_csv("graphs/sprint_3/Admissions_discharges_excldeaths.csv")
 
 
+# Graph with absolute values
+(combined_april_excldeaths %>% 
+    filter(day_dummy <= ymd("0004-04-30") & day_dummy >= ymd("0004-02-01") & sourcedest != "all") %>% 
+    ggplot(aes(x = day_dummy, y = sevendayavg, color = year, group = year)) +
+    geom_vline(xintercept = ymd("0004-03-17"), color = THF_dark_grey, linetype = "dashed", alpha = 0.3) +
+    geom_line() +
+    geom_point(size = 0.5) +
+    theme_bw() +
+    facet_wrap(vars(type, sourcedest), scales = "free", 
+               labeller = as_labeller(c("adm" = "Admissions",
+                                        "disch" = "Discharges",
+                                        "ch" = "from/to care homes",
+                                        "other" = "from/to other dest."))) +
+    ylab("7 day rolling average") +
+    scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+    theme(axis.title.x = element_blank(),
+          legend.position = "right",
+          legend.justification = c(1,0),
+          axis.text = element_text(colour = THF_dark_grey),
+          axis.title = element_text(colour = THF_dark_grey),
+          axis.ticks = element_line(colour = THF_light_grey),
+          panel.grid = element_blank(),
+          panel.grid.major.y = element_line(colour = THF_50pct_light_grey),
+          legend.text = element_text(colour = THF_dark_grey),
+          legend.title = element_blank(),
+          plot.title = element_text(colour = "#005078"),
+          plot.subtitle = element_text(colour = "#005078"),
+          strip.text = element_text(colour = "#524c48"),
+          strip.background = element_blank()) +
+    expand_limits(y=0) +
+    labs(title = "Hospital admissions and discharges")) %>% 
+  ggsave("graphs/sprint_3/Admissions_discharges_excldeaths.png", ., width = 9, height = 6, dpi = 600)
+  
+# Graph with absolute values, showing 2015-2019 as mean
+  
+  combined_april_excldeaths_2020 <- combined_april_excldeaths %>% 
+    filter(day_dummy <= ymd("0004-04-30") & day_dummy >= ymd("0004-02-01") & sourcedest != "all" & year == "2020") 
+    
+(combined_april_excldeaths %>% 
+      filter(day_dummy <= ymd("0004-04-30") & day_dummy >= ymd("0004-02-01") & sourcedest != "all") %>% 
+      ggplot(aes(x = day_dummy, y = ref_2015_to_2019)) +
+      geom_vline(xintercept = ymd("0004-03-17"), color = THF_dark_grey, linetype = "dashed", alpha = 0.3) +
+      geom_line() +
+      geom_point(size = 0.5) +
+      geom_line(data = combined_april_excldeaths_2020, 
+                aes(x = day_dummy, y = sevendayavg),
+                color = THF_red) +
+      geom_point(data = combined_april_excldeaths_2020, 
+                 aes(x = day_dummy, y = sevendayavg),
+                 size = 0.5, color = THF_red) +
+      theme_bw() +
+      facet_wrap(vars(type, sourcedest), scales = "free", 
+                 labeller = as_labeller(c("adm" = "Admissions",
+                                          "disch" = "Discharges",
+                                          "ch" = "from/to care homes",
+                                          "other" = "from/to other dest."))) +
+      ylab("7 day rolling average") +
+      scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+      theme(axis.title.x = element_blank(),
+            axis.text = element_text(colour = THF_dark_grey),
+            axis.title = element_text(colour = THF_dark_grey),
+            axis.ticks = element_line(colour = THF_light_grey),
+            panel.grid = element_blank(),
+            panel.grid.major.y = element_line(colour = THF_50pct_light_grey),
+            plot.title = element_text(colour = "#005078"),
+            plot.subtitle = element_text(colour = "#005078"),
+            strip.text = element_text(colour = "#524c48"),
+            strip.background = element_blank()) +
+      expand_limits(y=0) +
+      labs(title = "Hospital admissions and discharges",
+           subtitle = "Black - mean 2015 to 2019, red - 2020")) %>% 
+    ggsave("graphs/sprint_3/Admissions_discharges_excldeaths2.png", ., width = 9, height = 6, dpi = 600)
+  
+
+# Graph indexed to average 2015-2019
 (combined_april_excldeaths  %>% 
+    filter(day_dummy <= ymd("0004-04-30") & day_dummy >= ymd("0004-02-01") & sourcedest != "all" & year == "2020") %>% 
     ggplot(aes(x = day_dummy, y = percent_2015_to_2019, color = sourcedest, group = sourcedest)) +
     geom_vline(xintercept = ymd("0004-03-17"), color = THF_dark_grey, linetype = "dashed", alpha = 0.3) +
     annotate("rect", xmin = ymd("0004-04-10"), xmax = ymd("0004-04-13"), 
@@ -416,10 +498,158 @@ combined_april_excldeaths %>%
           strip.text = element_text(colour = "#524c48"),
           strip.background = element_blank()) +
     annotate("text", x = ymd("0004-03-18"), y = 118, 
-             label = "NHS discharge\nguidance", color = THF_dark_grey, size = 2, hjust = 0) +
-    annotate("text", x = ymd("0004-04-21"), y = 120, 
-             label = "Easter 2020", color = THF_dark_grey, size = 2) +
+             label = "17 March,\nNHS discharge\nguidance", color = THF_dark_grey, size = 2, hjust = 0) +
+    annotate("text", x = ymd("0004-04-14"), y = 120, 
+             label = "10-13 April,\nEaster 2020", color = THF_dark_grey, size = 2, hjust = 0) +
     labs(title = "Hospital admissions and discharges in 2020", 
-         subtitle = "7-day moving averages, shown as a percentage of the mean of 2015-2019",
+         subtitle = "Seven day moving average,\nindexed to the average of the same period between 2015 and 2019",
          color = "Source/destination")) %>% 
-  ggsave("graphs/sprint_3/Admissions_discharges_excldeaths.png", ., width = 7, height = 4, dpi = 600)
+  ggsave("graphs/sprint_3/Admissions_discharges_excldeaths_rel20215-2019.png", ., width = 7, height = 4, dpi = 600)
+
+
+# Graph indexed to 2019 only
+(combined_april_excldeaths  %>%
+    filter(day_dummy <= ymd("0004-04-30") & day_dummy >= ymd("0004-02-01") & sourcedest != "all" & year == "2020") %>% 
+    ggplot(aes(x = day_dummy, y = percent_2019, color = sourcedest, group = sourcedest)) +
+    geom_vline(xintercept = ymd("0004-03-17"), color = THF_dark_grey, linetype = "dashed", alpha = 0.3) +
+    annotate("rect", xmin = ymd("0004-04-10"), xmax = ymd("0004-04-13"), 
+             ymin = -Inf, ymax = Inf, fill = THF_dark_grey, alpha = 0.3) +
+    
+    geom_line() +
+    geom_point(size = 0.5) +
+    theme_bw() +
+    facet_grid(.~type, scale = "free_y", 
+               labeller = as_labeller(c("adm" = "Admissions",
+                                        "disch" = "Discharges"))) +
+    ylab("Percent") +
+    scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+    scale_color_manual(values = c(THF_red, THF_50pct_light_blue), labels = c("from/to care home", "other source/destination")) +
+    
+    scale_y_continuous(breaks = seq(40, 100, by = 20)) +
+    theme(axis.title.x = element_blank(),
+          legend.position = "top",
+          legend.justification = c(1,0),
+          axis.text = element_text(colour = THF_dark_grey),
+          axis.title = element_text(colour = THF_dark_grey),
+          axis.ticks = element_line(colour = THF_light_grey),
+          panel.grid = element_blank(),
+          panel.grid.major.y = element_line(colour = THF_50pct_light_grey),
+          legend.text = element_text(colour = THF_dark_grey),
+          legend.title = element_blank(),
+          plot.title = element_text(colour = "#005078"),
+          plot.subtitle = element_text(colour = "#005078"),
+          strip.text = element_text(colour = "#524c48"),
+          strip.background = element_blank()) +
+    annotate("text", x = ymd("0004-03-18"), y = 118, 
+             label = "17 March,\nNHS discharge\nguidance", color = THF_dark_grey, size = 2, hjust = 0) +
+    annotate("text", x = ymd("0004-04-14"), y = 120, 
+             label = "10-13 April,\nEaster 2020", color = THF_dark_grey, size = 2, hjust = 0) +
+    labs(title = "Hospital admissions and discharges in 2020", 
+         subtitle = "Seven day moving average,\nindexed to the same period in 2019",
+         color = "Source/destination")) %>% 
+  ggsave("graphs/sprint_3/Admissions_discharges_excldeaths_rel2019.png", ., width = 7, height = 4, dpi = 600)
+
+# Ratio discharges to admissions
+(combined_april_excldeaths %>% 
+   filter(day_dummy <= ymd("0004-04-30") & day_dummy >= ymd("0004-02-01") & sourcedest != "all") %>% 
+   group_by(sourcedest, date, day_dummy) %>% 
+   mutate(disch_adm_ratio = sevendayavg[type == "disch"] / sevendayavg[type == "adm"]) %>% 
+      ggplot(aes(x = day_dummy, y = disch_adm_ratio, color = year, group = year)) +
+      geom_vline(xintercept = ymd("0004-03-17"), color = THF_dark_grey, linetype = "dashed", alpha = 0.3) +
+      geom_line() +
+      geom_point(size = 0.5) +
+      theme_bw() +
+      facet_wrap(vars(sourcedest), scales = "free", 
+                 labeller = as_labeller(c("ch" = "from/to care homes",
+                                          "other" = "from/to other dest."))) +
+      ylab("Discharges / admissions (ratio 7 day rolling averages)") +
+      scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+      theme(axis.title.x = element_blank(),
+            legend.position = "right",
+            legend.justification = c(1,0),
+            axis.text = element_text(colour = THF_dark_grey),
+            axis.title = element_text(colour = THF_dark_grey),
+            axis.ticks = element_line(colour = THF_light_grey),
+            panel.grid = element_blank(),
+            panel.grid.major.y = element_line(colour = THF_50pct_light_grey),
+            panel.grid.minor.y = element_line(colour = THF_50pct_light_grey),
+            legend.text = element_text(colour = THF_dark_grey),
+            legend.title = element_blank(),
+            plot.title = element_text(colour = "#005078"),
+            plot.subtitle = element_text(colour = "#005078"),
+            strip.text = element_text(colour = "#524c48"),
+            strip.background = element_blank()) +
+      expand_limits(y=0) +
+      labs(title = "Ratio discharges to admissions")) %>% 
+    ggsave("graphs/sprint_3/Admissions_discharges_excldeaths_ratio.png", ., width = 9, height = 4, dpi = 600)
+  
+  
+# During February, what % of admissions and discharges were from/to care homes?
+# and how does the absolute number compare to the 5-year average of 2015-2019
+
+admissions_april_excldeaths %>% 
+  bind_rows(discharges_april_excldeaths) %>%
+  filter(month(date) == 2) %>% 
+  group_by(type, year, sourcedest) %>% 
+  summarise(sevendayavg_sum = sum(sevendayavg),
+            value_sum = sum(value),
+            date_start = min(date),
+            date_end = max(date)) %>%  
+  group_by(type, year) %>% 
+  mutate(value_pct = 100* (value_sum / value_sum[sourcedest == "all"]),
+         sevendayavg_pct = 100* (sevendayavg_sum / sevendayavg_sum[sourcedest == "all"])) %>% 
+  group_by(type, sourcedest) %>% 
+  mutate(value_ref_2015_to_2019 = mean(value_sum[year %in% c(2015:2019)]),
+         value_percent_2015_to_2019 = round(100*value_sum / value_ref_2015_to_2019, 1)) %>% 
+  write_csv("graphs/sprint_3/Admissions_discharges_February_sum.csv")
+  
+# Same for March and April combined  
+
+admissions_april_excldeaths %>% 
+    bind_rows(discharges_april_excldeaths) %>%
+    filter(month(date) %in% c(3,4)) %>% 
+    group_by(type, year, sourcedest) %>% 
+    summarise(sevendayavg_sum = sum(sevendayavg),
+              value_sum = sum(value),
+              date_start = min(date),
+              date_end = max(date)) %>%  
+    group_by(type, year) %>% 
+    mutate(value_pct = 100* (value_sum / value_sum[sourcedest == "all"]),
+           sevendayavg_pct = 100* (sevendayavg_sum / sevendayavg_sum[sourcedest == "all"])) %>% 
+    group_by(type, sourcedest) %>% 
+    mutate(value_ref_2015_to_2019 = mean(value_sum[year %in% c(2015:2019)]),
+           value_percent_2015_to_2019 = round(100*value_sum / value_ref_2015_to_2019, 1)) %>%  
+      write_csv("graphs/sprint_3/Admissions_discharges_March-April_sum.csv")
+    
+# Between 17 March and 30 April
+  
+  admissions_april_excldeaths %>% 
+    bind_rows(discharges_april_excldeaths) %>%
+    filter(day_dummy >= ymd("0004-03-17") & day_dummy <= ymd("0004-04-30")) %>% 
+    group_by(type, year, sourcedest) %>% 
+    summarise(sevendayavg_sum = sum(sevendayavg),
+              value_sum = sum(value),
+              date_start = min(date),
+              date_end = max(date)) %>%  
+    group_by(type, year) %>% 
+    mutate(value_pct = 100* (value_sum / value_sum[sourcedest == "all"]),
+           sevendayavg_pct = 100* (sevendayavg_sum / sevendayavg_sum[sourcedest == "all"])) %>% 
+    group_by(type, sourcedest) %>% 
+    mutate(value_ref_2015_to_2019 = mean(value_sum[year %in% c(2015:2019)]),
+           value_percent_2015_to_2019 = round(100*value_sum / value_ref_2015_to_2019, 1)) %>%  
+    write_csv("graphs/sprint_3/Admissions_discharges_March17-April_sum.csv")
+  
+
+
+# Rgional and care home type aggregates -----------------------------------
+
+# Sums betweetn 17 March and 30 April
+
+
+discharges_regional <- read_csv("data/sprint_3/2020_06_22_2/CH_discharges_regional_Mar-April.csv",
+                                col_types = ) 
+
+discharges_regional %>% 
+  group_by(year, dischtype) %>% 
+  summarise(count = sum(count))
+
