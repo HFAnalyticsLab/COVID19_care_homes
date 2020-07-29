92e869288be8152c4159385d987ecae436d4ad41
fiona-grimm <Fiona.grimm@health.org.uk>
2020-07-29 12:52:47 +0000

    Create additional vars, fix bug in flagging admissions, update visualisations

--- src/sprint_3/04_permresidents_admissions_discharges.R
+++ src/sprint_3/04_permresidents_admissions_discharges.R
@@ -2,7 +2,6 @@
 # Hospital admissions / discharges of care home residents with MPI flag
 ####
 
-
 library(tidyverse)
 library(tidylog)
 library(janitor)
@@ -18,9 +17,7 @@ source("functions.R")
 # 2017 - now
 
 # Residents and care home characteristics
-chres <- readRDS(str_c(Rds_data_path, 'mmpiss_bymonth_chchars_2017-2020.Rds'))  %>% 
-  mutate(chr_at_spellstart = 1,
-         chr_at_spellend = 1)
+chres <- readRDS(str_c(Rds_data_path, 'mmpiss_bymonth_chchars.Rds'))  
 
 
 # Hospital inpatient spells
@@ -31,10 +28,10 @@ chres_apcs <- read_csv(str_c(raw_data_path, "April 2020/chres_apcs.csv"),
                                         dischdate = col_date(format = "%d/%m/%Y")))
 
 chres_apcs <- chres_apcs %>% 
-  filter(spellstartdate >= ymd("2017-01-01"))
+  filter(spellstartdate >= ymd("2015-01-01"))
 
-saveRDS(chres_apcs, str_c(Rds_data_path, 'chres_apcs_2017-2020.Rds'))
-chres_apcs <- readRDS(str_c(Rds_data_path, 'chres_apcs_2017-2020.Rds'))
+#saveRDS(chres_apcs, str_c(Rds_data_path, 'chres_apcs_2017-2020.Rds'))
+#chres_apcs <- readRDS(str_c(Rds_data_path, 'chres_apcs_2017-2020.Rds'))
 
 
 # Match with care home resident characteristics ---------------------------
@@ -45,18 +42,32 @@ chres_apcs <- readRDS(str_c(Rds_data_path, 'chres_apcs_2017-2020.Rds'))
 
 # Flagging who was a resident when admitted
 
+chres_dt_spellstart <-  chres %>% 
+  ungroup() %>% 
+  select(pid, assigned_tnrchid, ch_nursing, ch_residential, ch_beds_filled, ch_is_open,
+         datefrom, dateto) %>% 
+  mutate(chr_at_spellstart = 1) %>% 
+  as.data.table()
+
+setkey(chres_dt_spellstart, pid, datefrom, dateto)
+
+chres_dt_spellend <-  chres %>% 
+  ungroup() %>% 
+  select(pid, assigned_tnrchid, ch_nursing, ch_residential, ch_beds_filled, ch_is_open,
+         datefrom, dateto) %>% 
+  mutate(chr_at_spellend = 1) %>% 
+  as.data.table()
+
+setkey(chres_dt_spellend, pid, datefrom, dateto)
+
+
+# Duplicate columns (need separate period start and period end for interval join)
 chres_apcs <- as.data.table(chres_apcs)
 chres_apcs[, spellstartdate2 := spellstartdate]
 chres_apcs[, spellenddate2 := spellenddate]
 
-chres_dt <-  chres %>% 
-  ungroup() %>% 
-  select(pid, assigned_tnrchid, ch_nursing, ch_residential, ch_beds_filled, ch_is_open,
-         datefrom, dateto, chr_at_spellstart, chr_at_spellend) %>% 
-  as.data.table()
-setkey(chres_dt, pid, datefrom, dateto)
 
-chres_apcs <- foverlaps(chres_apcs, chres_dt, 
+chres_apcs <- foverlaps(chres_apcs, chres_dt_spellstart, 
                         by.x = c("pid", "spellstartdate", "spellstartdate2"),
                         by.y = c("pid", "datefrom", "dateto"))
 
@@ -67,7 +78,7 @@ chres_apcs <- chres_apcs %>%
          ch_is_open_spellstart = "ch_is_open")
 
 
-chres_apcs <- foverlaps(chres_apcs, chres_dt, 
+chres_apcs <- foverlaps(chres_apcs, chres_dt_spellend, 
                         by.x = c("pid", "spellenddate", "spellenddate2"),
                         by.y = c("pid", "datefrom", "dateto"))
 
@@ -84,172 +95,124 @@ chres_apcs <-  chres_apcs %>%
   select(-spellstartdate2, -spellenddate2)
 
 
-saveRDS(chres_apcs, str_c(Rds_data_path, 'chres_apcs_2017-2020_mmpiss_matched.Rds'))
-chres_apcs <- readRDS(str_c(Rds_data_path, 'chres_apcs_2017-2020_mmpiss_matched.Rds'))
+# Creating location type variables
+chres_apcs <-  chres_apcs %>%
+  mutate(ch_admission = ifelse(chr_at_spellstart == 1 & (is.na(admincat) | admincat != 2), 1, 0),
+         ch_discharge = ifelse(chr_at_spellend == 1 & (is.na(admincat) | admincat != 2) & 
+                                 !is.element(dischdest, 
+                                             c(98, 51, 52, 53, 30, 37, 38, 48, 49, 50, 66, 84, 87)), 1 , 0),
+         ch_discharge = ifelse(dischdest == 79, NA, ch_discharge))
+
+# Creating admission type variables
+chres_apcs <-  chres_apcs %>%
+  mutate(adm_type = case_when(admmthd %in% c("21", "22", "23", "24", "25", "28", "2A", "2B", "2C", "2D") ~ "emergency",
+                              admmthd %in% c("11", "12", "13") ~ "elective",
+                              TRUE ~ "other"))
+
+# Care home type
+chres_apcs <-  chres_apcs %>%
+  mutate(ch_type_spellstart = case_when(!is.na(ch_nursing_spellstart) & ch_nursing_spellstart == 1 ~ "nursing",
+                                        !is.na(ch_residential_spellstart) & ch_residential_spellstart == 1 ~ "residential",
+                                        TRUE ~ "unknown"),
+         ch_type_spellend = case_when(!is.na(ch_nursing_spellend) & ch_nursing_spellend == 1 ~ "nursing",
+                                        !is.na(ch_residential_spellend) & ch_residential_spellend == 1 ~ "residential",
+                                        TRUE ~ "unknown"))
+         
+         
+# COVID primary diagnosis
+chres_apcs <-  chres_apcs %>%
+  mutate(covid_prim = ifelse(!is.na(primdiag) & (primdiag == "U071" | primdiag == "U072"), 1, 0))
+         
+ 
+View(chres_apcs %>%  
+       filter(spellstartdate == ymd("2020-01-15")) %>%  
+       select(pid, spellstartdate, assigned_tnrchid_spellstart, ch_nursing_spellstart, admmthd, adm_type,  datefrom_ch_spellstart, chr_at_spellstart, ch_admission, ch_type_spellstart, spellenddate, assigned_tnrchid_spellend, chr_at_spellend, ch_discharge, dischdest, admincat, datefrom_ch_spellend, dateto_ch_spellend, ch_type_spellend,  ch_nursing_spellend))
+        
+saveRDS(chres_apcs, str_c(Rds_data_path, 'chres_apcs_2015-2020_mmpiss_matched.Rds'))
+chres_apcs <- readRDS(str_c(Rds_data_path, 'chres_apcs_2015-2020_mmpiss_matched.Rds'))
 
 
-#Counting admissions -----------------------------------------------------
+# Counting daily admissions and discharges ------------------------------------
 
 
 chr_admissions <- chres_apcs %>% 
-  filter(chr_at_spellstart == 1 & admincat != 2) %>% 
+  filter(ch_admission == 1) %>% 
   group_by(spellstartdate) %>% 
   summarise(adm_all = n(),
-            adm_elective = sum(elective == 1),
-            adm_emergency = sum(emergency == 1)) %>% 
-  mutate(adm_all_7dayavg = sevendayavg(adm_all),
-         adm_elective_7dayavg = sevendayavg(adm_elective),
-         adm_emergency_7dayavg = sevendayavg(adm_emergency),
-         year = year(spellstartdate),
-         day_dummy = `year<-`(spellstartdate, 0001))
-
-write_csv(chr_admissions, "../results_sprint3/Chr_admissions.csv")
-
-
-(chr_admissions %>% 
-    ggplot(aes(x = day_dummy, y = adm_all_7dayavg, group = factor(year),
-               color = factor(year))) +
-    scale_x_date(date_labels = "%b", date_breaks = "1 month") +
-    geom_line() +
-    geom_point(size = 1) +
-    theme_bw() +
-    theme(panel.grid.minor.x = element_line(),
-          axis.title.x =  element_blank())+
-    ylab("all hospital admissions (7-day rolling average)") +
-    labs(title = "Hospital admissions", subtitle = "Care home residents flagged in the MPI")) %>% 
-  ggsave("../results_sprint3/Chr_admissions.png",., device = "png")
-
-
-chr_admissions_nursing <- chres_apcs %>% 
-  filter(chr_at_spellstart == 1 & admincat != 2) %>% 
-  group_by(spellstartdate, ch_nursing_spellstart) %>% 
-  summarise(adm_all = n(),
-            adm_elective = sum(elective == 1),
-            adm_emergency = sum(emergency == 1)) %>% 
-  group_by(ch_nursing_spellstart) %>% 
-  mutate(adm_all_7dayavg = sevendayavg(adm_all),
-         adm_elective_7dayavg = sevendayavg(adm_elective),
-         adm_emergency_7dayavg = sevendayavg(adm_emergency),
-         year = year(spellstartdate),
-         day_dummy = `year<-`(spellstartdate, 0001))
-
-write_csv(chr_admissions_nursing, "../results_sprint3/Chr_admissions_chnursing.csv")
-
-
-(chr_admissions_nursing %>%
- filter(!is.na(ch_nursing_spellstart)) %>% 
-  ggplot(aes(x = day_dummy, y = adm_all_7dayavg, group = factor(year),
-             color = factor(year))) +
-  facet_grid(ch_nursing_spellstart ~., labeller = as_labeller(c(`0` = "Residential home",
-                                                           `1` = "Nursing home"))) +
-  scale_x_date(date_labels = "%b", date_breaks = "1 month") +
-  geom_line() +
-  geom_point(size = 1) +
-  theme_bw() +
-  theme(panel.grid.minor.x = element_line(),
-        axis.title.x =  element_blank())+
-  ylab("all hospital admissions (7-day rolling average)") +
-  labs(title = "Hospital admissions", subtitle = "Care home residents flagged in the MPI")) %>% 
-  ggsave("../results_sprint3/Chr_admissions_chnursing.png",., device = "png")
-
-# Counting discharges -----------------------------------------------------
+            adm_emergency = sum(adm_type == "emergency"),
+            adm_elective = sum(adm_type == "elective"),
+            adm_other = sum(adm_type == "other"),
+            adm_nursing = sum(ch_type_spellstart == "nursing"),
+            adm_residential = sum(ch_type_spellstart == "residential"),
+            adm_unknown = sum(ch_type_spellstart == "unknown"),
+            adm_covid = sum(covid_prim == 1)) 
 
 chr_discharges <- chres_apcs %>% 
-  filter(chr_at_spellend == 1 & !is.element(dischdest, c(79, 98, 51, 52, 53, 30, 37, 38, 48, 49, 50, 66, 84, 87)) 
-         & admincat != 2) %>% 
+  filter(ch_discharge == 1) %>% 
   group_by(spellenddate) %>% 
-  summarise(disch = n()) %>% 
-  mutate(disch_7dayavg = sevendayavg(disch),
-         year = year(spellenddate),
-         day_dummy = `year<-`(spellenddate, 0001))
+  summarise(disch_all = n(),
+            disch_nursing = sum(ch_type_spellstart == "nursing"),
+            disch_residential = sum(ch_type_spellstart == "residential"),
+            disch_unknown = sum(ch_type_spellstart == "unknown")) 
 
-write_csv(chr_discharges, "../results_sprint3/Chr_discharges.csv")
+chr_adm_disch <- chr_admissions %>% 
+  left_join(chr_discharges, by = c("spellstartdate" = "spellenddate")) %>% 
+  rename(date = "spellstartdate") %>% 
+  pivot_longer(-date, names_to = "temp", values_to = "count") %>% 
+  separate("temp", into = c("type", "group"), sep = "_") %>% 
+  group_by(type, group) %>% 
+  arrange(date) %>% 
+  mutate(sevendayavg = sevendayavg(count),
+         year = year(date),
+         day_dummy = `year<-`(date, 0004))
+
+chr_adm_disch <- chr_adm_disch %>% 
+  group_by(type, group, day_dummy) %>% 
+  mutate(ref_sevendayavg_2015_to_2019 = mean(sevendayavg[year %in% c(2015:2019)]),
+         pct_sevendayavg_2015_to_2019 = round(100*(sevendayavg/ ref_sevendayavg_2015_to_2019), 1))
+
+write_csv(chr_adm_disch, "../results_sprint3/Chr_perm_admissions_discharges.csv")
 
 
-(chr_discharges %>% 
-    ggplot(aes(x = day_dummy, y = disch_7dayavg, group = factor(year),
+
+# Visualisation  ----------------------------------------------------------
+
+(chr_adm_disch %>% 
+   filter(group == "all") %>% 
+    ggplot(aes(x = day_dummy, y = sevendayavg, group = factor(year),
                color = factor(year))) +
     scale_x_date(date_labels = "%b", date_breaks = "1 month") +
     geom_line() +
     geom_point(size = 1) +
     theme_bw() +
+    facet_wrap("type") +
     theme(panel.grid.minor.x = element_line(),
           axis.title.x =  element_blank())+
-    ylab("all hospital discharges (7-day rolling average)") +
-    labs(title = "Hospital discharges", subtitle = "Care home residents flagged in the MPI")) %>% 
-  ggsave("../results_sprint3/Chr_discharges.png",., device = "png")
+    ylab("7-day rolling average") +
+    labs(title = "Hospital admissions and discharges", subtitle = "Care home residents flagged in the MPI")) %>% 
+  ggsave("../results_sprint3/Chr_admissions_discharges.png",., device = "png")
 
-
-chr_discharges_nursing <- chres_apcs %>% 
-  filter(chr_at_spellend == 1 & !is.element(dischdest, c(79, 98, 51, 52, 53, 30, 37, 38, 48, 49, 50, 66, 84, 87)) & dischmthd != 4) %>% 
-  group_by(spellenddate, ch_nursing_spellend) %>% 
-  summarise(disch = n()) %>% 
-  group_by(ch_nursing_spellend) %>% 
-  mutate(disch_7dayavg = sevendayavg(disch),
-         year = year(spellenddate),
-         day_dummy = `year<-`(spellenddate, 0001))
-
-write_csv(chr_discharges_nursing, "../results_sprint3/Chr_discharges_chnursing.csv")
-
-
-(chr_discharges_nursing %>% 
-    filter(!is.na(ch_nursing_spellend)) %>% 
-    ggplot(aes(x = day_dummy, y = disch_7dayavg, group = factor(year),
+(chr_adm_disch %>% 
+    filter(group == "all" & day_dummy %within% interval(ymd("0004-02-01"), ymd("2020-04-30"))) %>% 
+    ggplot(aes(x = day_dummy, y = sevendayavg, group = factor(year),
                color = factor(year))) +
-    facet_grid(ch_nursing_spellend ~., labeller = as_labeller(c(`0` = "Residential home",
-                                                                  `1` = "Nursing home"))) +
     scale_x_date(date_labels = "%b", date_breaks = "1 month") +
     geom_line() +
     geom_point(size = 1) +
     theme_bw() +
+    facet_wrap("type") +
     theme(panel.grid.minor.x = element_line(),
           axis.title.x =  element_blank())+
-    ylab("all hospital discharges (7-day rolling average)") +
-    labs(title = "Hospital discharges", subtitle = "Care home residents flagged in the MPI")) %>% 
-  ggsave("../results_sprint3/Chr_discharges_chnursing.png",., device = "png")
+    ylab("7-day rolling average") +
+    labs(title = "Hospital admissions and discharges", subtitle = "Care home residents flagged in the MPI")) %>% 
+  ggsave("../results_sprint3/Chr_admissions_discharges_Jan_Apr.png",., device = "png")
 
 
-# Admissions vs discharges ------------------------------------------------
-
-combined_nursing <- chr_admissions_nursing %>% 
-  left_join(chr_discharges_nursing, by = c("spellstartdate" = "spellenddate", "ch_nursing_spellstart" =
-                                             "ch_nursing_spellend"))
-
-(combined_nursing %>% 
-  select(spellstartdate, ch_nursing_spellstart, adm_all_7dayavg, disch_7dayavg) %>% 
-  filter(!is.na(ch_nursing_spellstart)) %>% 
-  pivot_longer(c(-spellstartdate, -ch_nursing_spellstart), names_to = "type", values_to = "value") %>% 
-  mutate(year = year(spellstartdate),
-         day_dummy = `year<-`(spellstartdate, 0001)) %>% 
-  ggplot(aes(x = day_dummy, y = value, group = factor(type),
-             color = factor(type))) +
-  facet_grid(ch_nursing_spellstart ~ year, labeller = as_labeller(c(`0` = "Residential home",
-                                                              `1` = "Nursing home",
-                                                              `2017` = "2017",
-                                                              `2018` = "2018",
-                                                              `2019` = "2019",
-                                                              `2020` = "2020"))) +
-  scale_x_date(date_labels = "%b", date_breaks = "1 month") +
-  geom_line() +
-  geom_point(size = 1) +
-  theme_bw() +
-  theme(panel.grid.minor.x = element_line(),
-        axis.title.x =  element_blank())+
-  ylab("all hospital discharges (7-day rolling average)") +
-  labs(title = "Hospital discharges", subtitle = "Care home residents flagged in the MPI")) %>% 
-  ggsave("../results_sprint3/Chr_admissions_discharges_chnursing.png",., device = "png", width = 10)
-         
-
-
-
-combined <- chr_admissions %>% 
-  left_join(chr_discharges, by = c("spellstartdate" = "spellenddate"))
-
-(combined %>% 
-    select(spellstartdate, adm_all_7dayavg, disch_7dayavg) %>% 
-    mutate(year = year(spellstartdate),
-           day_dummy = `year<-`(spellstartdate, 0001)) %>% 
-    ggplot(aes(x = day_dummy, y = disch_7dayavg/adm_all_7dayavg, group = factor(year),
+(chr_adm_disch %>% 
+    filter(group == "all") %>% 
+    group_by(date, day_dummy, year) %>% 
+    mutate(ratio =  sevendayavg[type == "disch"]/sevendayavg[type == "adm"]) %>% 
+    ggplot(aes(x = day_dummy, y = ratio, group = factor(year),
                color = factor(year))) +
     scale_x_date(date_labels = "%b", date_breaks = "1 month") +
     geom_line() +
@@ -260,4 +223,32 @@ combined <- chr_admissions %>%
     ylab("discharges/admissions (7-day rolling averages)") +
     labs(title = "Discharges/admissions ratio", subtitle = "Care home residents flagged in the MPI")) %>% 
   ggsave("../results_sprint3/Chr_discharges_to_admissions_ratio.png",., device = "png", width = 10)
+
+
+chr_adm_disch_2020 <- chr_adm_disch %>% 
+  filter(group == "all" & year == 2020 & day_dummy %within% interval(ymd("0004-02-01"), ymd("2020-04-30")))
+
+
+
+
+(chr_adm_disch_2020 %>% 
+    ggplot(aes(x = day_dummy, y = pct_sevendayavg_2015_to_2019, color = type)) +
+    scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+    geom_line() +
+    geom_point(size = 1) +
+    theme_bw() +
+    theme(panel.grid.minor.x = element_line(),
+          axis.title.x =  element_blank())+
+    ylab("Percent") +
+    labs(title = "Hospital admissions and discharges", subtitle = "Care home residents flagged in the MPI
+         \nSeven-day moving averages, indexed to the average of the same period in 2015-2019")) %>% 
+  ggsave("../results_sprint3/Chr_admissions_discharges.png",., device = "png")
+
+
+
+
+
+
+
+
 
